1e236fda65ebffb19eae8123a6abb417f7b7d1c9
Martin Kamír <martin.k.kamir@gmail.com>
2022-02-28 17:19:36 +0100

    navbar & variables - refactor/adding props

--- src/react-components/Navbar.js
+++ src/react-components/Navbar.js
@@ -2,12 +2,12 @@ import logo from '../imgs/logo.svg';
 import React, { useState } from 'react';
 
 export default function Navbar() {
-  const [active, setActive] = useState(false);
-  const [list, setList] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+  const [isShow, setisShow] = useState(false);
 
   function toggle() {
-    setActive(!active);
-    setList(true);
+    setIsOpen(!isOpen);
+    setisShow(true);
   }
 
   return (
@@ -27,15 +27,15 @@ export default function Navbar() {
           <a href="#">Contact</a>
         </li>
       </ul>
-      <button className={`nav__hamburger ${active ? 'nav__hamburger--open' : ''}`} onClick={toggle}>
+      <button className={`nav__hamburger ${isOpen ? 'nav__hamburger--open' : ''}`} onClick={toggle}>
         <i></i>
         <i></i>
         <i></i>
       </button>
-      {list && (
+      {isShow && (
         <ul
           className={`nav__dropdown-menu ${
-            active ? 'nav__dropdown-menu--open' : 'nav__dropdown-menu--hidden'
+            isOpen ? 'nav__dropdown-menu--open' : 'nav__dropdown-menu--hidden'
           }`}
         >
           <li className="nav__link">
@@ -54,7 +54,7 @@ export default function Navbar() {
       )}
       <div
         className={`nav__dropdown-overlay ${
-          active ? 'nav__dropdown-overlay--open' : 'nav__dropdown-overlay--hidden'
+          isOpen ? 'nav__dropdown-overlay--open' : 'nav__dropdown-overlay--hidden'
         }`}
         onClick={toggle}
       ></div>
